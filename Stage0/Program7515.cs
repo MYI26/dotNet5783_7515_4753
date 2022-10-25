b63f5bd3fd8da305eca4b495d770b0fd486d183e
Yona Mimoun <yona26.mimoun@gmail.com>
2022-10-25 13:50:19 +0300

    first commit by Yona

---
+++ Stage0/Program7515.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        //public string {0};
+        static void Main(string[] args)
+        {
+            Welcome9715();
+            Welcome4753();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome4753();
+        private static void Welcome9715()
+        {
+            Console.WriteLine("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", username);
+        }
+    }
+}
