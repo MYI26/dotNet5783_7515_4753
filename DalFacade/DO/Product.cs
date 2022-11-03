63816d59e46bfea6dd8a9743d6eaba33379bebb8
trixalphi Mad <eliaou4562200@gmail.com>
2022-11-04 00:31:40 +0200

    creation of Order and product

---
+++ DalFacade/DO/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
+
+/// <summary>
+/// Structure for Product on sale resource
+/// </summary>
+
+public struct Product
+{
+
+    /// <summary>
+    /// Unique ID of product
+    /// </summary>
+
+    public int ID { get; set; }
+
+
+    /// <summary>
+    /// Descriptivename of product
+    /// </summary>
+
+    public string Name { get; set; }
+
+
+    /// <summary>
+    /// Current sell price of product
+    /// </summary>
+
+    public int Price { get; set; }
+
+
+    /// <summary>
+    /// Category of product in the store product list
+    /// </summary>
+
+    public Category Category { get; set; }
+
+
+    /// <summary>
+    /// Category of product in the store product list
+    /// </summary>
+
+    public int InStock { get; set; }
+
+}
+
