777c686b245126d2bbb1072dc0be47b910e121fc
trixalphi Mad <eliaou4562200@gmail.com>
2022-11-06 19:37:10 +0200

    construction : DalProduct / DalOrderItem / DalOrder

--- DalList/DalOrder.cs
+++ DalList/DalOrder.cs
@@ -1,6 +1,94 @@
-﻿
+﻿using DO;
+using System;
+
 namespace Dal;
 
 public class DalOrder
 {
+
+    public int add(Order p1)
+    {
+
+        for (int i = 0; i < 40; i++)
+        {
+            if (DataSource.tabOrderItem[i].ProductID == p1.ID)
+            {
+                throw new Execution("the OrderItem already exist")
+                return 0;
+
+            }
+        }
+
+        else
+        {
+
+            int i = 40;
+            DataSource.tabOrderItem[i] = p1;
+            i++;
+
+
+            return p1.ProductID;
+        }
+    }
+
+    public void delet(int id)
+    {
+
+        for (int i = 0; i < 40; i++)
+        {
+            if (DataSource.tabOrderItem[i].ProcuctID == id)
+            {
+
+                DataSource.tabOrderItem[i] = null;
+
+            }
+        }
+
+
+    }
+
+    public void update(Order P1)
+    {
+
+        for (int i = 0; i < 40; i++)
+        {
+            if (DataSource.tabOrderItem[i].ProductID == P1.ID)
+            {
+
+                DataSource.tabOrderItem[i] = P1;
+
+            }
+        }
+
+    }
+
+    public Order get(int id)
+    {
+        for (int i = 0; i < 40; i++)
+        {
+            if (DataSource.tabOrderItem[i].ProductID == id)
+            {
+
+                return DataSource.tabOrderItem[i];
+
+            }
+        }
+
+    }
+
+    public void get()
+    {
+
+        for (int i = 0; i < 40; i++)
+        {
+
+
+
+
+              public override string ToString() => $@"
+        Product ID={DataSource.tabOrderItem[i].ProductID}: {DataSource.tabOrderItem[i].OrderID}, 
+    	Price: {DataSource.tabProduct[i].Price}
+    	Amount in stock: {DataSource.tabProduct[i].InStock} ";
+
+
 }
--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -1,6 +1,106 @@
-﻿
+﻿using DO;
+using System;
+
 namespace Dal;
 
 public class DalProduct
 {
+
+    public int add(Product p1)
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (DataSource.tabProduct[i].ID == p1.ID)
+            {
+                throw new Execution("the product already exist")
+                return 0;
+
+            }
+        }
+
+        else {
+
+            int i = 10;
+            DataSource.tabProduct[i] = p1;
+            i++;
+
+
+            return p1.ID;
+        }
+    }
+
+    public void delet(int id)
+    {
+
+        for (int i = 0; i < 50; i++)
+        {
+            if (DataSource.tabProduct[i].ID == id)
+            {
+
+                DataSource.tabProduct[i] = null;
+
+            }
+        }
+
+
+    }
+
+    public void update(Product P1)
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (DataSource.tabProduct[i].ID == P1.ID)
+            {
+
+                DataSource.tabProduct[i] = P1;
+
+            }
+        }
+
+    }
+
+    public Product get(int id)
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            if (DataSource.tabProduct[i].ID == id)
+            {
+
+                return DataSource.tabProduct[i];
+
+            }
+        }
+
+    }
+
+    public void get() {
+
+        for (int i = 0; i < 50; i++) {
+
+
+
+
+              public override string ToString() => $@"
+        Product ID={DataSource.tabProduct[i].ID}: {DataSource.tabProduct[i].Name}, 
+        category - {DataSource.tabProduct[i].Category}
+    	Price: {DataSource.tabProduct[i].Price}
+    	Amount in stock: {DataSource.tabProduct[i].InStock} ";
+
+                 }
+
+
 }
+        
+
+     
+
+
+
+
+
+
+
+
+
