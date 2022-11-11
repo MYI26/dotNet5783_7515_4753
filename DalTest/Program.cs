7fdf7c5d809f2bc440286e45df340ac2db4efdbb
trixalphi Mad <eliaou4562200@gmail.com>
2022-11-11 15:03:59 +0200

    Construction Main

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DO;
+using Dal;
+using System;
+using System.Net.WebSockets;
+using System.Diagnostics;
+
+namespace Program
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+            Enter:
+            1) Product
+            2) Order Item
+            3) Order    
+            0) exite
+            ");
+
+            int choises = 0;
+
+            string s = Console.Readline();
+            int b = int.Parse(s);
+            choises = b;
+
+            while (choises != 0)
+            {
+                if (choises == 1)
+                {
+
+                    Console.WriteLine(@"
+            Enter:
+            a) Add Product
+            b) Print Product
+            c) Print All Product 
+            d) Update Product
+            e) delete Product
+                    ");
+                }
+                if (choises == 2)
+                {
+
+                    Console.WriteLine(@"
+            Enter:
+            a) Add Order Item
+            b) Print Order Item
+            c) Print All Order Item 
+            d) Update Order Item
+            e) delete Order Item
+                    ");
+                }
+
+                if (choises == 3)
+                {
+
+                    Console.WriteLine(@"
+            Enter:
+            a) Add Order
+            b) Print Order
+            c) Print All Order 
+            d) Update Order
+            e) delete Order
+                    ");
+
+
+                }
+            }
+        }
+    }   
+}
