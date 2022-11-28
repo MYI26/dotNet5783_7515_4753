36d531d6aa30732ed8878c875d3aab944f97f8a7
Yona Mimoun <yona26.mimoun@gmail.com>
2022-11-28 19:12:10 +0200

    add entities of BL

---
+++ BL/BO/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace BO
+{
+    public class Cart
+    {
+        public string? CustomerName { set; get; }
+        public string? CustomerEmail { set; get; }
+        public string? CustomerAdress { set; get; }
+        public List<OrderItem?>? Cart.Items { set; get; }
+    }
+}
